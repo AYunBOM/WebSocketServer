a832e7748cae4ad2288940d654a99bc92abb115a
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2023-10-08 19:28:35 +0900

    server_log

--- server/wsServer.py
+++ server/wsServer.py
@@ -9,11 +9,23 @@ import threading
 host = '0.0.0.0'  # 모든 IP 주소에서 연결 허용
 port = 8080       # 사용할 포트 번호
 
+server_file = open("server_log.txt", 'w')
+
 thread_num = 0              # 클라이언트 번호 초기값 설정
-system_clock = 0            # 서버 0~600초 누적시간
+system_clock = ""           # 서버 0~600초 누적시간
 time_ls = [0 ,0 ,0 ,0]      # 클라이언트 각각의 시간적립 리스트(스레드번호가 인덱스 번호이자 클라이언트 번호가 됨)
 result_sum = 0
 
+# 시간을 리얼타임으로 변환
+def real_time(time): 
+    minute = "{}".format(time // 60)
+    second = "{}".format(time % 60)
+    result = "{}:{}".format(minute.zfill(2), second.zfill(2))
+    # 예) 3초 => 00:03 / 100초 => 01:40
+    return result
+    
+
+# 임의의 문제 출제&정답
 def random_question():
     first_num = random.randint(1, 100)
     second_num = random.randint(1, 100)
@@ -24,49 +36,58 @@ def random_question():
     question = "{} {} {} {} {}".format(first_num, operator1, second_num, operator2, third_num)
 
     answer = int(eval(question))
-
-    question += " = 정답을 입력해 주세요.\n"
+    
+    question += " = "
 
     return question, answer
 
-
+# 클라이언트에게 문제 출제&정답체크
 def client_handler(client_socket, thread_num):
     global system_clock, result_sum
 
-    print("클라이언트{} 시작".format(thread_num))  # 스레드 번호 출력
-    
     question, answer = random_question()
     
-    system_clock = time_ls[thread_num]
+    system_clock = real_time(time_ls[thread_num])
 
-    while system_clock < 600:
-        question += ",{},{}".format(system_clock, thread_num) # [3 + 4 + 5 = ?], [4]
+    while time_ls[thread_num] < 600:
+        question += ",{}".format(time_ls[thread_num]) # [3 + 4 + 5 = ?], [4] # [3 + 4 + 5 = ?], [4]
          
-        client_socket.send(question.encode('utf-8'))
+        server_file.write("{} > 클라이언트 {}에게 문제를 출제합니다.".format(system_clock, thread_num))
 
-        data = client_socket.recv(1024).decode('utf-8')
+        client_socket.send(question.encode("utf-8"))
+        data = client_socket.recv(1024).decode("utf-8")
         
         client_time, client_ans  = map(int, data.split())
         time_ls[thread_num] = client_time
          
-        system_clock = time_ls[thread_num]
+        system_clock = real_time(time_ls[thread_num])
         print(system_clock)
 
+        # 문제를 맞췄을 시, 임의의 시간동안 대기 후 새 문제 출제
         if client_ans == answer :
             delay = random.randint(1, 5)
-            print("맞췄습니다. 다음 대기 시간은 {} 입니다".format(delay))
-            time.sleep(delay)
-            time_ls[thread_num] += delay
-            system_clock = time_ls[thread_num]
-            result_sum += client_ans
-            question, answer = random_question()
 
+            server_file.write("{} > '클라이언트{}'가 답을 맞췄습니다. 정답:{}".format(system_clock, thread_num, answer))
+            server_file.write("{} > {}초 뒤 '클라이언트{}'에게 새 문제를 출제합니다.".format(system_clock, delay, thread_num))
+
+            time.sleep(delay) # 임의로 지정한 대기 시간
+            time_ls[thread_num] += delay # 시간 업데이트(대기 시간 추가)
+            system_clock = real_time(time_ls[thread_num]) # 전체 시간 업데이트
+
+            result_sum += client_ans # 클라이언트가 푼 문제의 답 최종 합계
+            
+            
+            question, answer = random_question()
+        
+        # 문제를 틀렸을 시, 같은 문제 재전송
         elif client_ans != answer :
             question = question.split(',')[0]
-            print("틀렸습니다. 클라이언트{}에게 문제를 다시 보냅니다.".format(thread_num))
-            continue
-        
-    print("'클라이언트{}'의 접속을 종료합니다.".format(thread_num))
+
+            server_file.write("{} > '클라이언트{}'의 답이 틀렸습니다. 문제를 재전송합니다.".format(system_clock, thread_num))
+
+            continue   
+         
+    server_file.write("{} > '클라이언트{}'의 접속을 종료합니다.".format(system_clock, thread_num))
 
 # 소켓 생성
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -76,12 +97,11 @@ server_socket.bind((host, port))
 
 # 클라이언트로부터 연결 대기
 server_socket.listen(4) # 4개의 연결을 동시에 처리
-print("서버가 {}:{}에서 실행 중입니다.".format(host, port))
+server_file.write("서버가 {}:{}에서 실행 중입니다.".format(host, port))
 
 # 클라이언트와 연결 수락
-while system_clock < 600:
+while True:
     client_socket, client_address = server_socket.accept()
-    print("'클라이언트{}'의 연결이 수락되었습니다.".format(client_address))
     client_thread = threading.Thread(
         target=client_handler, args=(client_socket, thread_num)
     )
@@ -89,9 +109,14 @@ while system_clock < 600:
 
     delay_time = int(client_socket.recv(1024).decode("utf-8"))
     time_ls[thread_num] = delay_time
+    system_clock = real_time(time_ls[thread_num])
+        
+    server_file.write("{} > '클라이언트 {}' 연결 완료.\n".format(system_clock, thread_num))
 
     thread_num += 1
-    
-print("최종 합계 : {}".format(result_sum))
+
+server_file.write("최종 합계 : {}".format(result_sum))
+
+server_file.close()
 client_socket.close()
 server_socket.close()
